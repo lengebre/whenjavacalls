e9aaf920a69c9a5ce165debd91d5937b0be36e68
Mo <mo@mo.local>
2022-04-18 20:28:03 -0400

    Sample Interface

--- src/Hello.java
+++ src/Hello.java
@@ -1,7 +1,13 @@
-public class Hello {
+public class Hello implements SampleInterface {
+    
 
     void hello() {
         System.out.println("Hello, World!");
+    }
+
+    @Override
+    public void sample() {
+        System.out.println("Sample interface implemented");
     }
     
 }
---
+++ src/SampleInterface.java
@@ -0,0 +1,4 @@
+public interface SampleInterface {
+    public void sample();
+    
+}
