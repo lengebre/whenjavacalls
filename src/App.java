8641c97e365a3d3833c69d69e9d590ef2c524a50
Mo <mo@mo.local>
2022-04-18 20:26:14 -0400

    Hello method test

--- src/App.java
+++ src/App.java
@@ -1,5 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        Hello h = new Hello();
+        h.hello();
     }
 }
---
+++ src/Hello.java
@@ -0,0 +1,7 @@
+public class Hello {
+
+    void hello() {
+        System.out.println("Hello, World!");
+    }
+    
+}
